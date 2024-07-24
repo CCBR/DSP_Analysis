f4baecdbfc1414425541d52bef7b8a9e8c39e245
Ned Cauley <ned.cauley@nih.gov>
2024-07-24 13:27:52 -0400

    Updates for QC including added AOI count graphs

--- DSP_QC_functions.R
+++ DSP_QC_functions.R
@@ -1,4 +1,119 @@
 
+initialize_object <- function(dcc.files,
+                              pkc.files,
+                              annotation.file,
+                              annotation.sheet.name = "Template",
+                              sample.id.field.name = "Sample_ID",
+                              roi.field.name = "roi",
+                              panel.field.name = "panel",
+                              slide.field.name = "slide name", 
+                              class.field.name = "class", 
+                              region.field.name = "region", 
+                              segment.field.name = "segment",
+                              area.field.name = "area",
+                              nuclei.field.name = "nuclei", 
+                              segment.id.length = 4){
+    
+    # load all input data into a GeoMX object
+    object <-
+      readNanoStringGeoMxSet(
+        dccFiles = dcc.files,
+        pkcFiles = pkc.files,
+        phenoDataFile = annotation.file,
+        phenoDataSheet = annotation.sheet.name,
+        phenoDataDccColName = sample.id.field.name,
+        protocolDataColNames = roi.field.name,
+        experimentDataColNames = panel.field.name
+      )
+
+    object <-
+      readNanoStringGeoMxSet(
+        dccFiles = dcc.files,
+        pkcFiles = pkc.files,
+        phenoDataFile = annotation.file,
+        phenoDataSheet = annotation.sheet.name,
+        phenoDataDccColName = sample.id.field.name, 
+        experimentDataColNames = panel.field.name
+      )
+    
+    # Check the column names for required fields exist in the annotation
+    
+    required.field.names = c(slide.field.name, 
+                             class.field.name, 
+                             region.field.name, 
+                             segment.field.name, 
+                             roi.field.name)
+    given.field.names = colnames(sData(object))
+    
+    # Check each of the required fields for correct naming
+    for (field in required.field.names) {
+      if (!(field %in% given.field.names)) {
+        stop(
+          paste0(
+            field,
+            " is not found in the annotation sheet field names.\n"
+          )
+        )
+      }
+    }
+    
+    # Check for the optional fields
+    optional.field.names = c("area", "nuclei")
+    for (field in optional.field.names) {
+      if (!(field %in% given.field.names)) {
+        warning(
+          paste0(
+            field,
+            " is not found in the annotation and will not be considered \n"
+          )
+        )
+      }
+    }
+    
+    # Rename all of the required columns based on user parameters in data
+    colnames(object@phenoData@data)[colnames(object@phenoData@data) == slide.field.name] = "slide_name"
+    colnames(object@phenoData@data)[colnames(object@phenoData@data) == class.field.name] = "class"
+    colnames(object@phenoData@data)[colnames(object@phenoData@data) == region.field.name] = "region"
+    colnames(object@phenoData@data)[colnames(object@phenoData@data) == segment.field.name] = "segment"
+    colnames(object@phenoData@data)[colnames(object@phenoData@data) == roi.field.name] = "roi"
+    
+    # Rename all of the required columns based on user parameters in metadata
+    rownames(object@phenoData@varMetadata)[rownames(object@phenoData@varMetadata) == slide.field.name] = "slide_name"
+    rownames(object@phenoData@varMetadata)[rownames(object@phenoData@varMetadata) == class.field.name] = "class"
+    rownames(object@phenoData@varMetadata)[rownames(object@phenoData@varMetadata) == region.field.name] = "region"
+    rownames(object@phenoData@varMetadata)[rownames(object@phenoData@varMetadata) == segment.field.name] = "segment"
+    rownames(object@phenoData@varMetadata)[rownames(object@phenoData@varMetadata) == roi.field.name] = "roi"
+    
+    # Rename optional columns if they are present
+    colnames(object@phenoData@data)[colnames(object@phenoData@data) == area.field.name] = "area"
+    colnames(object@phenoData@data)[colnames(object@phenoData@data) == nuclei.field.name] = "nuclei"
+    rownames(object@phenoData@varMetadata)[rownames(object@phenoData@varMetadata) == area.field.name] = "area"
+    rownames(object@phenoData@varMetadata)[rownames(object@phenoData@varMetadata) == nuclei.field.name] = "nuclei" 
+    
+    # Reformat to remove spaces and dashes in the main annotation columns
+    annotation.columns <- c("class", "region", "segment", "slide_name")
+    
+    for(column in annotation.columns){
+      pData(object)[[column]] <- gsub("\\s+", "", pData(object)[[column]])
+      pData(object)[[column]] <- gsub("-", "", pData(object)[[column]])
+    }
+    
+    # Establish the segment specific IDs
+    pData(object)$segmentID <- paste0(substr(pData(object)$class, 1, segment.id.length),
+                                      "|",
+                                      substr(pData(object)$region, 1, segment.id.length),
+                                      "|",
+                                      substr(pData(object)$segment, 1, segment.id.length),
+                                      "|", 
+                                      substr(pData(object)$slide_name, 1, segment.id.length), 
+                                      "|", 
+                                      sData(object)$roi)
+
+    
+    return(object)
+  
+}
+
 # Set up the MA plot table
 make_MA <- function(contrast.field, 
                     condition.label, 
