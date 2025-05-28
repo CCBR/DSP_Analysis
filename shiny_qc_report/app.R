ad0a260fb78e8016a51b7dcfe7b1b82075864169
Ned Cauley <ned.cauley@nih.gov>
2025-05-28 16:48:19 -0400

    Initial setup for distribution plot

--- shiny_qc_report/app.R
+++ shiny_qc_report/app.R
@@ -6,9 +6,10 @@ library(GeomxTools)
 source("PCA_plot.R")
 
 # Load the input data
-data.folder <- "/Users/cauleyes/CPTR/DSP_Analysis/test_datasets/Human_Kidney/qc_report/"
-counts.data <- read.csv(paste0(data.folder, "human_kidney_test_quantile_norm_counts.csv"))
-annotation.data <- read.csv(paste0(data.folder, "human_kidney_test_annotation.csv"))
+data.folder <- "/Users/cauleyes/CPTR/CPTR-9_Roberto_Weigert/2025_whole_experiment/qc_report/"
+counts.data <- read.csv(paste0(data.folder, "CPTR_9_whole_experiment_quantile_norm_counts.csv"))
+annotation.data <- read.csv(paste0(data.folder, "CPTR_9_whole_experiment_annotation.csv"))
+#filtered.object <- readRDS(paste0(data.folder, "human_kidney_test_filtered_object.RDS"))
 
 # Define the column names to offer
 annotation.cols <- colnames(annotation.data %>% 
